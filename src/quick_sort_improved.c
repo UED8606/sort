8643028112efe3e3f9d3e432d9d65c8004890580
Ueda Kazuki <ueda.k.ao@m.titech.ac.jp>
2019-06-25 17:14:11 +0900

    completed

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -18,6 +18,26 @@ void swap(int *p, int *q){
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    if(n <= 1) return;
+    int i,j,eq,pivot;
+    pivot = A[0];
+    for(i = j = 1; i < n; i++){
+        if(A[i] < pivot){
+            swap(A+i, A+j);
+            j++;
+        }
+    }
+    eq = j;
+    for(i = j = eq; i < n; i++){
+        if(A[i] == pivot){
+            swap(A+i, A+j);
+            j++;
+        }
+    }
+    A[0] = A[eq-1];
+    A[eq-1] = pivot;
+    quick_sort(A,eq-1);
+    quick_sort(A+j,n-j);
 }
 
 int main(){
